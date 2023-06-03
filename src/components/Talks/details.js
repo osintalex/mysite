e7b69d507886f080f0eb04ae1c58dfb3535af2e9
Alexander Darby <63904626+osintalex@users.noreply.github.com>
2023-06-03 12:28:43 +0100

    feat: update landing, talks, work

--- src/components/Talks/details.js
+++ src/components/Talks/details.js
@@ -6,5 +6,16 @@ export const talkDetails = [
     description:
       "These days, it seems like everyone and their mother wants a TAXII server. But how do you actually build one quickly and well?",
     slidesLink: "https://cryptpad.fr/file/#/2/file/uiNS9iFTnCAb6iK9d2cR78I3/",
+    recordingLink: "https://www.youtube.com/watch?v=vv2EA3IapQ8",
+  },
+  {
+    date: "2023-05-28",
+    title: "Hack Your Own Code",
+    description: `Is your code hard to hack? 
+    Sure, code scans and lints are helpful but they don't guarantee that your code is secure. 
+    In fact, there is only one good way to answer that question: hack your own code.  
+    But we're software engineers, not hackers. So how do we do that?`,
+    slidesLink:
+      "https://drive.google.com/file/d/1QhmHJMdgtzATrbwCYRcJr51WXPxOt1Gw",
   },
 ];
